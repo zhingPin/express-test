53dbb82b0dc68ff0e944443812e73e999ca0630f
Gary Gee <crypto2doe@gmail.com>
2025-01-26 10:46:26 +0000

    testing mongo connection

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require("express");
-const { connectToDatabase } = require("./utils/connectToDatabase");
-const app = express();
+import { connectToDatabase } from "./utils/connectToDatabase";
+import express from "express";
 
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Subscribe to jon dough");
 });
-// connectToDatabase();
 
 app.listen(port, () => {
-  `Server started on port ${port}`;
+  console.log(`Server started on port ${port}`);
 });
