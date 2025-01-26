031d7ad8b3e4023258a58dd648af7a3cc907817e
Gary Gee <crypto2doe@gmail.com>
2025-01-26 11:51:01 +0000

    removed warnings

--- utils/connectToDatabase.js
+++ utils/connectToDatabase.js
@@ -17,7 +17,7 @@ const DB = process.env.DATABASE.replace(
 
 // Connect to MongoDB
 mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB, {})
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.error("DB connection failed:", err));
 
