dc3e229805f94cc49da3323a1e53ab74283c364b
Gary Gee <crypto2doe@gmail.com>
2025-01-26 14:45:57 +0000

    adding custom routes

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
-const express = require("express");
+import express from "express";
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8080;
