f63e9f2fde393297c93290aeabbeb61c9f4d4d08
Gary Gee <crypto2doe@gmail.com>
2025-01-26 10:32:11 +0000

    testing mongo connection

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+(async () => {
+  try {
+    await connectToDatabase();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    const errorMessage =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    console.error("Failed to connect to MongoDB:", errorMessage);
+    process.exit(1); // Exit process if DB connection fails
+  }
+})();
+
 app.get("/", (req, res) => {
   res.send("Subscribe to jon dough");
 });
-connectToDatabase();
+// connectToDatabase();
 
 app.listen(port, () => {
   `Server started on port ${port}`;
