aaa4b9582c722f8622af2c6e062f07fb6fce9749
Gary Gee <crypto2doe@gmail.com>
2025-01-26 14:20:18 +0000

    adding custom routes

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import app from "../apiContext/app";
+
+// Initialize dotenv in development mode
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: "./config.env" });
+}
+
 const app = express();
 
-// // Initialize dotenv in development mode
-// if (process.env.NODE_ENV !== "production") {
-//   dotenv.config({ path: "./config.env" });
-// }
+// Replace <PASSWORD> dynamically in the connection string
+const DB = process.env.DATABASE;
 
-// // Replace <PASSWORD> dynamically in the connection string
-// const DB = process.env.DATABASE;
+// console;
 
 // // Connect to MongoDB
 // mongoose
@@ -23,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("Subscribe to jon dough");
 });
 
-// Start the server by using the app imported from app.ts
+// Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
