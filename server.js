13d44ca88655592cd8173a585e20bc2a516539d9
Gary Gee <crypto2doe@gmail.com>
2025-01-26 10:30:05 +0000

    testing mongo connection

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Subscribe to jon dough");
 });
+connectToDatabase();
 
 app.listen(port, () => {
   `Server started on port ${port}`;
