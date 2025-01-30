547836d16bc7443b52eb90e19f62754589ee876b
Gary Gee <crypto2doe@gmail.com>
2025-01-30 16:01:41 +0000

    added helmet

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(cors());
 app.options("*", cors());
+
+// Body parser: Limit request payload size
+app.use(express.json({ limit: "100kb" }));
+
+app.use(helmet()); // Secure HTTP headers
 
 export default app;
