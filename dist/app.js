caf5358e574282ac7cd09867a9f80108e5313d48
Gary Gee <crypto2doe@gmail.com>
2025-08-03 21:14:07 +0100

    Restore deleted folders/files

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 // ROUTES
 import assistantRouter from "./api/aiAssistant/routes/assistantRouter.js";
+import threadRouter from "./api/aiAssistant/routes/threadRouter.js";
+import messageRouter from "./api/aiAssistant/routes/messageRouter.js";
+import chatRouter from "./api/aiAssistant/routes/chatRouter.js";
 // ERROR HANDLER
 import AppError from "./api/helpers/appError.js";
 import { globalErrorHandler } from "./api/helpers/globalErrorHandler.js";
@@ -25,14 +28,14 @@ app.use(cors());
 app.options("*", cors());
 app.use(mongoSanitize()); // Prevent NoSQL injection attacks
 app.post("/submit", [body("input").trim().escape()], // Use an array for middleware
-(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return; // Ensure function execution stops here
-    }
-    res.send("Data is clean");
-});
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ errors: errors.array() });
+            return; // Ensure function execution stops here
+        }
+        res.send("Data is clean");
+    });
 app.use(hpp({
     whitelist: [],
 }));
@@ -66,9 +69,9 @@ app.get("/docs", (req, res) => {
     res.send("Documentation.");
 });
 app.use("/api/v1/assistants", assistantRouter);
-// app.use("/api/v1/thread", threadRouter);
-// app.use("/api/v1/message", messageRouter);
-// app.use("/api/v1/chat", chatRouter);
+app.use("/api/v1/thread", threadRouter);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/chat", chatRouter);
 // app.use("/api/v1/health-check", healthCheckRouter);
 app.get("/", (req, res) => {
     res.send("Welcome my the Express Server!");
